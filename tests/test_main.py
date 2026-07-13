28ecbfb7ddeb542b12e9cdc57c31a2f73ad5b623
Beza <bezawitbiazine@gmail.com>
2026-07-13 13:26:33 -0500

    fix: change response test structure

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status": "healthy", "message": "API is operational"}
+    assert response.json() == {"message": "Welcome to the Medical Telegram Warehouse Analytical API. Navigate to /docs for testing."}
